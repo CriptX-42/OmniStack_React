df0b1785d9d8df740e554913f5b3d0f3014321c1
ricardo <ricardo-svc@live.com>
2020-02-16 15:48:52 -0300

    Entendendo o conceito de propriedade (props)

--- web/src/App.js
+++ web/src/App.js
@@ -4,14 +4,21 @@ import Header from './Header'
 
 
 /*
-* *  Componente: Um bloco isolado de HTML, CSS , JS o qual não interfere no restante da aplicação
-* *  Estado: 
-* *  Propriedade: 
+  * *  Componente: Um bloco isolado de HTML, CSS , JS o qual não interfere no restante da aplicação
+  * *  Estado: Informações que um componente PAi (App.js) passa para o filho (Header.js)
+  * *  Propriedade: 
 */
 
 function App() {
   return (
-    <Header/>
+    /*
+      * *  <></> === Fragment
+    */
+    <> 
+      <Header title="Titulo 1"/>
+      <Header title="Titulo 2"/>
+      <Header title="Titulo 4"/>
+    </>
   );
 }
 
