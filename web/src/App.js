64e7d36aa899d8f6a8eef6210e2970ec53b28b4a
ricardo <ricardo-svc@live.com>
2020-02-16 16:57:07 -0300

    Fazendo estrutura inicial da aplicação, aplicando css inicial

--- web/src/App.js
+++ web/src/App.js
@@ -1,32 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+import './Global.css'
 
-import Header from './Header'
-
-
-/*
-  * *  Componente: Um bloco isolado de HTML, CSS , JS o qual não interfere no restante da aplicação
-  * *  Propriedade: Informações que um componente PAi (App.js) passa para o filho (Header.js)
-  * *  Estado: Informações mantidas pelo componente (imutabilidade)
-*/
 
 function App() {
-  
-  const [counter, setCounter] = useState(0) 
-
-
-  function incrementCounter(){
-    setCounter(counter + 1)
-  }
-
   return (
-    /*
-      * *  <></> === Fragment
-    */
-    <> 
-    <Header title="Olá"></Header>
-      <h1>Contador: {counter} </h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
